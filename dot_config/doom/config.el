cf807d6f8294943fcb810e90b0e2d8d1c166cc0c
Neil Munro <neil@elixirsoftware.co.uk>
2025-10-08 15:33:21 +0100

    Added doom config

---
+++ dot_config/doom/config.el
@@ -0,0 +1,43 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+
+(setq doom-theme 'doom-monokai-pro)
+(define-key key-translation-map (kbd "M-3") (kbd "#"))
+(define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
+(define-key evil-visual-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
+(define-key evil-normal-state-map (kbd "C-s") 'evil-numbers/dec-at-pt)
+(define-key evil-visual-state-map (kbd "C-s") 'evil-numbers/dec-at-pt)
+
+; Projectile
+(setq projectile-project-search-path '("~/quicklisp/local-projects/" "~/dev/" "~/Projects/"))
+
+; Fonts
+(setq doom-font (font-spec :family "Fira Code" :size 16 :weight 'Regular))
+
+(after! lsp-mode
+  (lsp-register-client
+    (make-lsp-client :new-connection (lsp-stdio-connection "python3"
+                                      (lambda () '("localhost" "5678")))
+                     :major-modes '(python-mode)
+                     :server-id 'pylsp-docker)))
+
+(after! lsp-mode
+  (setq lsp-diagnostics-provider :auto  ; Use lsp-mode for diagnostics
+        lsp-pylsp-plugins-mypy-enabled t  ; Ensure mypy is enabled
+        lsp-pylsp-plugins-mypy-live-mode nil  ; Disable live mode if it's too performance-intensive
+        lsp-pylsp-plugins-mypy-args '("--ignore-missing-imports")))  ; Example of adding extra mypy args
+
+;; Load dir-locals from emacs_config if it exists
+;; (let ((alt-dir-locals (expand-file-name "emacs_config/.dir-locals.el" (projectile-project-root))))
+;;   (when (file-exists-p alt-dir-locals)
+;;     (load-file alt-dir-locals)))
+
+(after! lsp-mode
+  (setq lsp-disabled-clients '(pylsp-docker)))
+
+;; (use-package! json-mode
+;;   :mode "\\.json\\'")
---
+++ dot_config/doom/custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages '(lsp-ui))
+ '(safe-local-variable-values
+   '((python-shell-interpreter . "/Users/nmunro/Projects/itrax/itracker/emacs_config/venv/bin/python")
+     (lsp-pyright-python-executable-cmd . "/Users/nmunro/Projects/itrax/itracker/emacs_config/venv/bin/python")
+     (python-shell-interpreter . "emacs_config/_host_venv/bin/python")
+     (lsp-pyright-python-executable-cmd . "emacs_config/_host_venv/bin/python")
+     (lsp-pyright-python-executable-cmd . "/Users/nmunro/Projects/itrax/itracker/emacs_config/_venv/bin/python")
+     (python-shell-interpreter-args . "")
+     (python-shell-interpreter . "/Users/nmunro/Projects/itrax/itracker/emacs_config/docker_python_repl.sh")
+     (lsp-enabled-clients pyright))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
